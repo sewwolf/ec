1fdba9543ce2dda052720d6b3369dec7bfbd5b35
sewwolf <sewwolf.gt@gmail.com>
2022-03-08 16:15:32 +0900

    カート機能実装

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,33 @@
+class CartsController < ApplicationController
+  def index
+    @total_price = my_cart.total_price
+    @items = my_cart.items
+  end
+
+  def create
+    if my_cart.add_item(product_id: params[:product_id], quantity: params[:quantity])
+    else
+  
+    end
+  end
+
+  def update
+    if my_cart.update_item(product_id: params[:product_id], quantity: params[:quantity])
+    else
+  
+    end
+  end
+
+  def destroy
+    if my_cart.delete_item(product_id: params[:product_id])
+    else
+  
+    end
+  end
+
+  private
+
+  def my_cart
+    Cart.find(params[:cart_id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_220_103_138) do
-  create_table 'addresses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'postal_code', null: false
-    t.integer 'area', default: 0, null: false
-    t.string 'municipality', null: false
-    t.string 'street_number', null: false
-    t.string 'building_name'
-    t.boolean 'service_delivery', default: false
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_addresses_on_user_id'
+ActiveRecord::Schema.define(version: 2022_03_08_065011) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "area", default: 0, null: false
+    t.string "municipality", null: false
+    t.string "street_number", null: false
+    t.string "building_name"
+    t.boolean "service_delivery", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'password_digest', null: false
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.string 'kana_first_name', null: false
-    t.string 'kana_last_name', null: false
-    t.integer 'gender', default: 0, null: false
-    t.string 'telephone_number', null: false
-    t.date 'birthday', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'addresses', 'users'
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_items_on_cart_id"
+    t.index ["product_id"], name: "index_items_on_product_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "kana_first_name", null: false
+    t.string "kana_last_name", null: false
+    t.integer "gender", default: 0, null: false
+    t.string "telephone_number", null: false
+    t.date "birthday", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "addresses", "users"
+  add_foreign_key "items", "carts"
+  add_foreign_key "items", "products"
 end
