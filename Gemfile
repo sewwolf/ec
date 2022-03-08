eeebe2b8f7643a8514e560fbabb49b5561be20d6
sewwolf <sewwolf.gt@gmail.com>
2022-03-08 14:55:05 +0900

    ヘッダー・フッターの実装

--- Gemfile
+++ Gemfile
@@ -52,6 +52,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'pry-rails'
+gem 'rails-i18n'
 gem 'rubocop', require: false
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-gem 'rails-i18n'
--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -7,19 +7,20 @@ class UserAddress
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   with_options presence: true do
     validates :email
-    validates :password, format: { with: PASSWORD_REGEX , allow_blank: true}
+    validates :password, format: { with: PASSWORD_REGEX, allow_blank: true }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力して下さい', allow_blank: true }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力して下さい', allow_blank: true }
     validates :kana_first_name, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角で入力して下さい', allow_blank: true }
     validates :kana_last_name, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角で入力して下さい', allow_blank: true }
     validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'を正しい値で入力して下さい', allow_blank: true }
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含めた正しい値で入力して下さい', allow_blank: true }
+    validates :postal_code,
+              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含めた正しい値で入力して下さい', allow_blank: true }
     validates :municipality
     validates :street_number
   end
-    validates :birthday, presence: { message: '選択して下さい' }
-    validates :gender, numericality: { other_than: 0, message: '選択して下さい' }
-    validates :area, numericality: { other_than: 0, message: '選択して下さい' }
+  validates :birthday, presence: { message: '選択して下さい' }
+  validates :gender, numericality: { other_than: 0, message: '選択して下さい' }
+  validates :area, numericality: { other_than: 0, message: '選択して下さい' }
 
   def save
     user = User.create(email: email, password: password, first_name: first_name, last_name: last_name,
